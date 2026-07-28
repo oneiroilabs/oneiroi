2647be41aa4acc2702f581cc2153efe226946244
Kilian Strunz <kilianstrunz@gmx.de>
2026-07-28 16:05:55 +0200

    some more experiments implmenting equidistant sampling

--- oneiroi-wgpu/src/main.rs
+++ oneiroi-wgpu/src/main.rs
@@ -14,6 +14,13 @@ use glam::{Vec3, Vec4};
 use std::{num::NonZeroU64, str::FromStr};
 use wgpu::util::DeviceExt;
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct GpuSample {
+    position: Vec4,
+    tangent: Vec4,
+}
+
 fn main() {
     // Parse all arguments as floats. We need to skip argument 0, which is the name of the program.
     /* let arguments: Vec<f32> = std::env::args()
@@ -114,182 +121,109 @@ fn main() {
     // a buffer with some initial data.
     //
     // We use the `bytemuck` crate to cast the slice of f32 to a &[u8] to be uploaded to the GPU.
-    let segments = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: None,
+    let sample_count = 10000u32; // Number of equidistant points requested
+
+    // Buffer 1: Input Curve Segments
+    let segments_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Curve Segments Buffer"),
         contents: bytemuck::cast_slice(&curve.segments),
         usage: wgpu::BufferUsages::STORAGE,
     });
 
-    let samples = vec![0.0f32, 0.2, 0.4, 0.6, 0.8, 1.0];
-    let input_ts = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: None,
-        contents: bytemuck::cast_slice(&samples),
-        usage: wgpu::BufferUsages::STORAGE,
-    });
-
-    // Now we create a buffer to store the output data.
+    // Buffer 2: Dedicated GPU Output storage
     let output_data_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: None,
-        size: (samples.len() * 4 * 4) as u64,
+        label: Some("Output Samples Storage Buffer"),
+        size: (sample_count as usize * std::mem::size_of::<GpuSample>()) as u64,
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
         mapped_at_creation: false,
     });
 
-    // Finally we create a buffer which can be read by the CPU. This buffer is how we will read
-    // the data. We need to use a separate buffer because we need to have a usage of `MAP_READ`,
-    // and that usage can only be used with `COPY_DST`.
+    // Buffer 3: CPU Read-back download staging buffer
     let download_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: None,
-        size: (samples.len() * 4 * 4) as u64,
+        label: Some("Download Buffer"),
+        size: output_data_buffer.size(),
         usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
         mapped_at_creation: false,
     });
 
-    // A bind group layout describes the types of resources that a bind group can contain. Think
-    // of this like a C-style header declaration, ensuring both the pipeline and bind group agree
-    // on the types of resources.
-    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+    // Define Bind Group Configurations matching the shader layout
+    let bind_group_layout_0 = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         label: None,
-        entries: &[
-            // Input buffer
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    // This is the size of a single element in the buffer.
-                    min_binding_size: Some(NonZeroU64::new(80).unwrap()),
-                    has_dynamic_offset: false,
-                },
-                count: None,
+        entries: &[wgpu::BindGroupLayoutEntry {
+            binding: 0,
+            visibility: wgpu::ShaderStages::COMPUTE,
+            ty: wgpu::BindingType::Buffer {
+                ty: wgpu::BufferBindingType::Storage { read_only: true },
+                min_binding_size: None,
+                has_dynamic_offset: false,
             },
-            // Output buffer
-            /* wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Uniform {},
-                    // This is the size of a single element in the buffer.
-                    min_binding_size: Some(NonZeroU64::new(4).unwrap()),
-                    has_dynamic_offset: false,
-                },
-                count: None,
-            }, */
-        ],
+            count: None,
+        }],
     });
 
     let bind_group_layout_1 = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         label: None,
-        entries: &[
-            // Input buffer
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    // This is the size of a single element in the buffer.
-                    min_binding_size: Some(NonZeroU64::new(4).unwrap()),
-                    has_dynamic_offset: false,
-                },
-                count: None,
+        entries: &[wgpu::BindGroupLayoutEntry {
+            binding: 0,
+            visibility: wgpu::ShaderStages::COMPUTE,
+            ty: wgpu::BindingType::Buffer {
+                ty: wgpu::BufferBindingType::Storage { read_only: false },
+                min_binding_size: None,
+                has_dynamic_offset: false,
             },
-            // Output buffer
-            wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
-                    // This is the size of a single element in the buffer.
-                    min_binding_size: Some(NonZeroU64::new(16).unwrap()),
-                    has_dynamic_offset: false,
-                },
-                count: None,
-            },
-        ],
+            count: None,
+        }],
     });
 
-    // The bind group contains the actual resources to bind to the pipeline.
-    //
-    // Even when the buffers are individually dropped, wgpu will keep the bind group and buffers
-    // alive until the bind group itself is dropped.
-    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+    let bind_group_0 = device.create_bind_group(&wgpu::BindGroupDescriptor {
         label: None,
-        layout: &bind_group_layout,
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: segments.as_entire_binding(),
-            },
-            /* wgpu::BindGroupEntry {
-                binding: 1,
-                resource: output_data_buffer.as_entire_binding(),
-            }, */
-        ],
+        layout: &bind_group_layout_0,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: segments_buffer.as_entire_binding(),
+        }],
     });
 
     let bind_group_1 = device.create_bind_group(&wgpu::BindGroupDescriptor {
         label: None,
         layout: &bind_group_layout_1,
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: input_ts.as_entire_binding(),
-            },
-            wgpu::BindGroupEntry {
-                binding: 1,
-                resource: output_data_buffer.as_entire_binding(),
-            },
-        ],
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: output_data_buffer.as_entire_binding(),
+        }],
     });
 
-    // The pipeline layout describes the bind groups that a pipeline expects
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: None,
-        bind_group_layouts: &[Some(&bind_group_layout), Some(&bind_group_layout_1)],
+        bind_group_layouts: &[Some(&bind_group_layout_0), Some(&bind_group_layout_1)],
         immediate_size: 0,
     });
 
-    // The pipeline is the ready-to-go program state for the GPU. It contains the shader modules,
-    // the interfaces (bind group layouts) and the shader entry point.
     let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
         label: None,
         layout: Some(&pipeline_layout),
-        module: &module,
+        module: &device.create_shader_module(wgpu::include_wgsl!("shader.wgsl")),
         entry_point: Some("main"),
         compilation_options: wgpu::PipelineCompilationOptions::default(),
         cache: None,
     });
 
-    // The command encoder allows us to record commands that we will later submit to the GPU.
+    // Record and Dispatch workload execution steps
     let mut encoder =
         device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+    {
+        let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: None,
+            timestamp_writes: None,
+        });
+        compute_pass.set_pipeline(&pipeline);
+        compute_pass.set_bind_group(0, &bind_group_0, &[]);
+        compute_pass.set_bind_group(1, &bind_group_1, &[]);
 
-    // A compute pass is a single series of compute operations. While we are recording a compute
-    // pass, we cannot record to the encoder.
-    let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-        label: None,
-        timestamp_writes: None,
-    });
+        let workgroup_count = sample_count.div_ceil(64);
+        compute_pass.dispatch_workgroups(workgroup_count, 1, 1);
+    }
 
-    // Set the pipeline that we want to use
-    compute_pass.set_pipeline(&pipeline);
-    // Set the bind group that we want to use
-    compute_pass.set_bind_group(0, &bind_group, &[]);
-    compute_pass.set_bind_group(1, &bind_group_1, &[]);
-
-    // Now we dispatch a series of workgroups. Each workgroup is a 3D grid of individual programs.
-    //
-    // We defined the workgroup size in the shader as 64x1x1. So in order to process all of our
-    // inputs, we ceiling divide the number of inputs by 64. If the user passes 32 inputs, we will
-    // dispatch 1 workgroups. If the user passes 65 inputs, we will dispatch 2 workgroups, etc.
-    let workgroup_count = curve.segments.len().div_ceil(64);
-    compute_pass.dispatch_workgroups(workgroup_count as u32, 1, 1);
-
-    // Now we drop the compute pass, giving us access to the encoder again.
-    drop(compute_pass);
-
-    // We add a copy operation to the encoder. This will copy the data from the output buffer on the
-    // GPU to the download buffer on the CPU.
     encoder.copy_buffer_to_buffer(
         &output_data_buffer,
         0,
@@ -297,16 +231,7 @@ fn main() {
         0,
         output_data_buffer.size(),
     );
-
-    // We finish the encoder, giving us a fully recorded command buffer.
-    let command_buffer = encoder.finish();
-
-    // At this point nothing has actually been executed on the gpu. We have recorded a series of
-    // commands that we want to execute, but they haven't been sent to the gpu yet.
-    //
-    // Submitting to the queue sends the command buffer to the gpu. The gpu will then execute the
-    // commands in the command buffer in order.
-    queue.submit([command_buffer]);
+    queue.submit([encoder.finish()]);
 
     // We now map the download buffer so we can read it. Mapping tells wgpu that we want to read/write
     // to the buffer directly by the CPU and it should not permit any more GPU operations on the buffer.
@@ -326,17 +251,17 @@ fn main() {
     // We can now read the data from the buffer.
     let data = buffer_slice.get_mapped_range().unwrap();
     // Convert the data back to f32 via an aligned copy.
-    let result: Vec<Vec4> = bytemuck::allocation::pod_collect_to_vec(&data);
+    let result: Vec<GpuSample> = bytemuck::allocation::pod_collect_to_vec(&data);
 
-    let num_samples = samples.len();
-    let mut cpu_eval = Vec::with_capacity(num_samples);
-    for t in samples.into_iter() {
+    //let num_samples = samples.len();
+    //let mut cpu_eval = Vec::with_capacity(num_samples);
+    /* for t in sample_count.into_iter() {
         let vec = curve.evaluate(t);
         cpu_eval.push(Vec4::new(vec.x, vec.y, vec.z, 0.0));
-    }
+    } */
 
     // Print out the result.
     println!("Result: {result:?}");
 
-    assert_eq!(cpu_eval, result);
+    //assert_eq!(cpu_eval, result);
 }
--- oneiroi-wgpu/src/shader.wgsl
+++ oneiroi-wgpu/src/shader.wgsl
@@ -10,60 +10,129 @@ struct CubicNurbsSegmentCache {
     cumulative_length: f32,
 }
 
-@group(0) 
-@binding(0)
+@group(0) @binding(0)
 var<storage, read> segments: array<CubicNurbsSegmentCache>;
 
-@group(1)
-@binding(0)
-var<storage, read> input_t: array<f32>;
+// Output: Array of positions and tangents bundled as vec4s
+struct OutputSample {
+    position: vec4<f32>,
+    tangent: vec4<f32>,
+}                   
 
-@group(1)
-@binding(1)
-var<storage, read_write> output_points: array<vec3<f32>>;
+@group(1) @binding(0)
+var<storage, read_write> output_samples: array<OutputSample>;
 
-fn find_segment_idx(t: f32) -> u32 {
-    var num_segments = arrayLength(&segments);
-    if (num_segments == 0u) { return 0u; }
-    if (t <= segments[0u].t_start) { return 0u; }
-    if (t >= segments[num_segments - 1u].t_end) { return num_segments - 1u; }
+// 5-Point Gauss–Legendre constants
+const GAUSS_NODES = array<f32, 5>(0.0, -0.5384693, 0.5384693, -0.90617985, 0.90617985);
+const GAUSS_WEIGHTS = array<f32, 5>(0.5688889, 0.47862867, 0.47862867, 0.23692689, 0.23692689);
 
-    var low: u32 = 0u;
-    var high: u32 = num_segments;
+fn evaluate_tangent(seg_idx: u32, t: f32) -> OutputSample {
+    let segment = segments[seg_idx];
+    let dt = segment.t_end - segment.t_start;
+    let u = (t - segment.t_start) / dt;
+    
 
+
+    let p_hom = fma(
+        fma(fma(segment.coeff_col3, vec4<f32>(u), segment.coeff_col2), vec4<f32>(u), segment.coeff_col1),
+        vec4<f32>(u),
+        segment.coeff_col0
+    );
+
+    let dp_du = fma(
+        fma(segment.coeff_col3 * 3.0, vec4<f32>(u), segment.coeff_col2 * 2.0),
+        vec4<f32>(u),
+        segment.coeff_col1
+    );
+
+    let inv_dt = 1.0 / dt;
+    let a_xyz = p_hom.xyz;
+    let w = p_hom.w;
+    let da = dp_du.xyz * inv_dt;
+    let dw = dp_du.w * inv_dt;
+
+    let c_pos = a_xyz / w;
+    let c_vel = (da - dw * c_pos) / w;
+
+    var out: OutputSample;
+    out.position = vec4<f32>(c_pos, 1.0);
+    out.tangent = vec4<f32>(c_vel, 0.0);
+    return out;
+}
+
+fn length_inside_segment(seg_idx: u32, t_cutoff: f32) -> f32 {
+    let segment = segments[seg_idx];
+    let dt = t_cutoff - segment.t_start;
+    if (dt <= 1e-6) { return 0.0; }
+
+    var span_integral = 0.0;
+    for (var i = 0u; i < 5u; i++) {
+        let t = 0.5 * (dt * GAUSS_NODES[i] + (t_cutoff + segment.t_start));
+        let sample_eval = evaluate_tangent(seg_idx, t);
+        span_integral += GAUSS_WEIGHTS[i] * length(sample_eval.tangent.xyz);
+    }
+    return span_integral * 0.5 * dt;
+}
+
+@compute @workgroup_size(64)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    let idx = global_id.x;
+    let count = arrayLength(&output_samples);
+    if (idx >= count) { return; }
+
+    let num_segments = arrayLength(&segments);
+    let total_length = segments[num_segments - 1u].cumulative_length;
+
+    // Handle edges cleanly
+    if (idx == 0u) {
+        output_samples[idx] = evaluate_tangent(0u, segments[0u].t_start);
+        return;
+    }
+    if (idx == count - 1u) {
+        output_samples[idx] = evaluate_tangent(num_segments - 1u, segments[num_segments - 1u].t_end);
+        return;
+    }
+
+    let step = total_length / f32(count - 1u);
+    let target_s = f32(idx) * step;
+
+    // Binary search for segment matching target_s (analogous to partition_point)
+    var low = 0u;
+    var high = num_segments;
     while (low < high) {
         let mid = low + (high - low) / 2u;
-        if (t >= segments[mid].t_start) {
+        if (segments[mid].cumulative_length < target_s) {
             low = mid + 1u;
         } else {
             high = mid;
         }
     }
-    return low - 1u;
-}
+    let current_seg_idx = clamp(low, 0u, num_segments - 1u);
+    let segment = segments[current_seg_idx];
 
-@compute 
-@workgroup_size(64)
-fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-    let idx = global_id.x;
-    
-    if (idx >= arrayLength(&input_t)) { return; }
+    var seg_start_len = 0.0;
+    if (current_seg_idx > 0u) {
+        seg_start_len = segments[current_seg_idx - 1u].cumulative_length;
+    }
+    let s_local = target_s - seg_start_len;
 
-    let t = input_t[idx];
-    let seg_idx = find_segment_idx(t);
-    let segment = segments[seg_idx];
+    // Initial root guess
+    var t = segment.t_start + (s_local / segment.length) * (segment.t_end - segment.t_start);
 
-    let u = (t - segment.t_start) / (segment.t_end - segment.t_start);
-    
-    let p_hom = fma(
-        fma(
-            fma(segment.coeff_col3, vec4<f32>(u), segment.coeff_col2), 
-            vec4<f32>(u), 
-            segment.coeff_col1
-        ), 
-        vec4<f32>(u), 
-        segment.coeff_col0
-    );
+    // Newton-Raphson iteration loop
+    for (var iter = 0; iter < 2; iter++) {
+        let current_s_local = length_inside_segment(current_seg_idx, t);
+        let sample_eval = evaluate_tangent(current_seg_idx, t);
+        let speed = length(sample_eval.tangent.xyz);
 
-    output_points[idx] = p_hom.xyz / p_hom.w;
+        if (speed < 1e-5) { break; }
+
+        let delta_t = (current_s_local - s_local) / speed;
+        t -= delta_t;
+        t = clamp(t, segment.t_start, segment.t_end);
+
+        if (abs(delta_t) < 1e-5) { break; }
+    }
+
+    output_samples[idx] = evaluate_tangent(current_seg_idx, t);
 }
